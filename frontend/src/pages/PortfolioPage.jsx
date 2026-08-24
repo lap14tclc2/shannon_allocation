d540182497431fabc33802b646f14840c83c3657
lap14tclc2 <honaz140@gmail.com>
2026-08-24 21:13:12 +0700

    UX: remove redundant portfolio analysis layers

--- frontend/src/pages/PortfolioPage.jsx
+++ frontend/src/pages/PortfolioPage.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import PortfolioDashboardPage from './PortfolioDashboardPage.jsx';
-import PortfolioAssessmentEnhancer from '../components/PortfolioAssessmentEnhancer.jsx';
-import ReceivedDividendsPanel from '../components/ReceivedDividendsPanel.jsx';
-import MarketHistoryIndicator from '../components/MarketHistoryIndicator.jsx';
-import FundManagerReview from '../components/FundManagerReview.jsx';
-import '../received-dividends.css';
-import '../fund-manager-review.css';
+import VietnamesePortfolioDashboard from './VietnamesePortfolioDashboard.jsx';
 
 export default function PortfolioPage(props) {
-  const dashboard = props.dashboard || {};
-  const locale = props.locale || 'en';
-  return <>
-    <PortfolioDashboardPage {...props} />
-    <MarketHistoryIndicator dashboard={dashboard} locale={locale} />
-    <FundManagerReview dashboard={dashboard} locale={locale} />
-    <PortfolioAssessmentEnhancer dashboard={dashboard} locale={locale} />
-    <ReceivedDividendsPanel locale={locale} />
-  </>;
+  return <VietnamesePortfolioDashboard {...props} locale="vi" />;
 }
