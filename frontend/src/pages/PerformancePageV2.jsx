17c13cd0f3c22bfdb3c746d44ee21a744c06ad12
lap14tclc2 <honaz140@gmail.com>
2026-08-24 21:32:16 +0700

    chore: keep simplified performance entry stable

--- frontend/src/pages/PerformancePageV2.jsx
+++ frontend/src/pages/PerformancePageV2.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import PerformancePage from './PerformancePage.jsx';
 
 export default function PerformancePageV2(props) {
-  return <PerformancePage {...props} locale="vi" />;
+  return <PerformancePage {...props} />;
 }
