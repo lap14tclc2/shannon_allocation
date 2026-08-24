29ab6e7fc0f30bb3c197f0b2aa71091f6588ae6b
lap14tclc2 <honaz140@gmail.com>
2026-08-24 21:13:53 +0700

    UX: remove duplicate performance supplement

--- frontend/src/pages/PerformancePageV2.jsx
+++ frontend/src/pages/PerformancePageV2.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import PerformancePage from './PerformancePage.jsx';
-import PerformanceHistoryPanel from '../components/PerformanceHistoryPanel.jsx';
 
 export default function PerformancePageV2(props) {
-  const performance = props.performance || {};
-  const hasTrackedSeries = Array.isArray(performance.series) && performance.series.length > 0;
-  return <>
-    <PerformancePage {...props} />
-    {hasTrackedSeries && <div className="page performance-history-supplement">
-      <PerformanceHistoryPanel performance={performance} locale={props.locale || 'en'} />
-    </div>}
-  </>;
+  return <PerformancePage {...props} locale="vi" />;
 }
